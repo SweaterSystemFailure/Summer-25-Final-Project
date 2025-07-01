e07cc3705c31ed59faa1c0d5564981495e032232
Ben Fagan <benjamin.s.fagan@gmail.com>
2025-07-01 16:59:32 -0700

    populated Scope of Work agreement in SoW.txt

--- containers.cpp
+++ containers.cpp
@@ -10,6 +10,38 @@
 namespace gradebookContainers {
 	using namespace Helpers;
 
+	//Gradebook
+	void Gradebook::addStudent() {
+		Student newStudent;
+
+		do {
+			newStudent.setFirstName(stringValidator("Please enter the student's first name: "));
+			newStudent.setLastName(stringValidator("Please enter the student's last name: "));
+			newStudent.setPronouns(stringValidator("Please enter the student's pronouns: "));
+			newStudent.setAge(numericValidator("Please enter the student's age: ", 4, 19));
+			newStudent.setID(numericValidator("Please enter the student's ID number: ", 1, 9999));
+			newStudent.setSeat(stringValidator("Please enter the student's seat location: "));
+			newStudent.setNotes(stringValidator("Enter any additional notes for this student: "));
+			newStudent.printStudent();
+		} while (!userCheck("Does this look right to you ? [Y / N] ", "Great! Let's continue", "That's okay. Let's try again."));
+
+		students.push_back(newStudent);
+
+		std::cout << "Student " << newStudent.getFirstName() << " " << newStudent.getLastName()
+			<< " has been added successfully!" << std::endl;
+
+		if (userCheck("Would you like to add another student? ",
+			"Okay, let's add another",
+			"Returning to main menu.")) {
+			addStudent();
+		}
+		else {
+			mainMenu();
+		}
+	}
+
+
+	//Classroom
 	Classroom Classroom::createClassroom() {
 		Classroom myClassroom;
 
@@ -31,7 +63,9 @@ namespace gradebookContainers {
 
 		return myClassroom;
 	}
+	
 
+	//Student
 	//Mutators
 	void Student::setFirstName(const std::string& entry) {
 		firstName = entry;
@@ -78,32 +112,5 @@ namespace gradebookContainers {
 		return notes;
 	}
 
-	void Gradebook::addStudent() {
-		Student newStudent;
 
-		do {
-			newStudent.setFirstName(stringValidator("Please enter the student's first name: "));
-			newStudent.setLastName(stringValidator("Please enter the student's last name: "));
-			newStudent.setPronouns(stringValidator("Please enter the student's pronouns: "));
-			newStudent.setAge(numericValidator("Please enter the student's age: ", 4, 19));
-			newStudent.setID(numericValidator("Please enter the student's ID number: ", 1, 9999));
-			newStudent.setSeat(stringValidator("Please enter the student's seat location: "));
-			newStudent.setNotes(stringValidator("Enter any additional notes for this student: "));
-			newStudent.printStudent();
-		} while (!userCheck("Does this look right to you ? [Y / N] ", "Great! Let's continue", "That's okay. Let's try again."));
-
-		students.push_back(newStudent);
-
-		std::cout << "Student " << newStudent.getFirstName() << " " << newStudent.getLastName()
-			<< " has been added successfully!" << std::endl;
-
-		if (userCheck("Would you like to add another student? ",
-			"Okay, let's add another",
-			"Returning to main menu.")) {
-			addStudent();
-		}
-		else {
-			mainMenu();
-		}
-	}
 }
